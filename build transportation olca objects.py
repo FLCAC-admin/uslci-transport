99f12822aa3208a0ceafbb57cc9febf38c3dbe67
maqchristie <martin.christie@erg.com>
2025-10-10 09:58:54 -0400

    df_olca populated

--- build transportation olca objects.py
+++ build transportation olca objects.py
@@ -10,6 +10,14 @@ The distances associated with each transport mode within a commodity category
 are weighted by the total moass of that commodity transported via the
 respective transportation mode.
 
+This script only works after running 'commodity transport distances.py'
+
+TO-DO:
+- Ask questions 
+- Complete dqi in flow meta
+- fill out process meta
+
+
 '''
 
 
@@ -23,7 +31,16 @@ import yaml
 from esupy.util import make_uuid
 import os
 
-# *** add code to read in the yaml files for filling in metadata
+# working directory
+working_dir = Path(r'C:\Users\mchristie\Code\uslci-transport')
+
+# Load yaml file for flow meta data
+with open(working_dir / 'transport_flow_meta.yaml') as f:
+    meta = yaml.safe_load(f)
+
+# Load yaml file for process meta data
+with open(working_dir / 'transport_process_meta.yaml') as f:
+    process_meta = yaml.safe_load(f)
 
 # Read in CSV file created by 'commodity transport distances.py'
 csv_path = r'C:\Users\mchristie\Code\uslci-transport\Weighted Commodity Transport Distances.csv'
@@ -31,23 +48,22 @@ df_olca = pd.read_csv(csv_path)
 df_olca = df_olca.drop(columns=['Mass Shipped (kg)', 'Avg. Dist. Shipped (km)', 'Mass Frac. by Mode'])
 
 # Create empty df_olca that includes all schema requirements
-# For ref. flow in each process, create new flow?
-schema = ['ProcessID', # Where is this assigned
-          'ProcessCategory', # '48-49: Transportation and Warehousing / *** add a new category?? ***'
-          'ProcessName', # Commodity specific process names to be developed
-          'FlowUUID', # Use existing transport flow UUIDs in USLCI 
-          'FlowName', # Use existing transport flow names in USLCI
-          'Context', # Use mode specific flow category from transportation and warehousing
-          'IsInput', # true for flows receiving data from csv | isInput = false for single refFlow per process
-          'FlowType', # 'PRODUCT_FLOW'
+schema = ['ProcessID',
+          'ProcessCategory',
+          'ProcessName',
+          'FlowUUID', 
+          'FlowName',
+          'Context',
+          'IsInput', 
+          'FlowType', 
           'reference', 
-          'default_provider', # Define by transport mode in metadata file
-          'description', # Define within process metadata file
-          'amount', # Input values from CSV | ref. flow is 1 kg of commodity
-          'unit', # Input values = kg*km | ref. flow = kg
-          'avoided_product', # false
-          'exchange_dqi', # define in meta data file
-          'location'] # US
+          'default_provider',
+          'default_provider_name',
+          'amount',
+          'unit',
+          'avoided_product',
+          'exchange_dqi',
+          'location']
 
 # Add schema columns to df_olca
 for column in schema:
@@ -58,11 +74,160 @@ for column in schema:
 df_olca['amount'] = df_olca['Weighted Dist. Shipped (km)']
 df_olca.drop('Weighted Dist. Shipped (km)', axis=1, inplace=True)
 
-# Add values that are the same for all inputs
-### Reference flows have not been added yet
+
+#%% Add values for inputs ###
 df_olca['IsInput'] = True
 df_olca['reference'] = False
 df_olca['unit'] = 'kg*km'
+df_olca['ProcessName'] = 'Transport, average, ' + df_olca['Commodity'].str.lower()
+df_olca['ProcessID'] = df_olca['ProcessName'].apply(make_uuid)
 
-# Add reference flows 
+# Map flow name based on transport mode mapping to uslci in transport_flow_meta.yaml
+df_olca['FlowName'] = df_olca['Transport Mode'].map(
+    {k: v['FlowName'] for k, v in meta['Mode'].items()})
+
+# Map flow uuid based on transport mode mapping to uslci in transport_flow_meta.yaml
+df_olca['FlowUUID'] = df_olca['Transport Mode'].map(
+    {k: v['FlowUUID'] for k, v in meta['Mode'].items()})
+
+# Map default provider name based on transport mode mapping to uslci in transport_flow_meta.yaml
+df_olca['default_provider_name'] = df_olca['Transport Mode'].map(
+    {k: v['ProcessName'] for k, v in meta['Mode'].items()})
+
+# Map default provider uuid based on transport mode mapping to uslci in transport_flow_meta.yaml
+df_olca['default_provider'] = df_olca['Transport Mode'].map(
+    {k: v['DefaultProviderUUID'] for k, v in meta['Mode'].items()})
+
+
+#%% Create new flows for the reference flow of each process ###
+
+# Get unique commodities
+unique_commodities = df_olca['Commodity'].unique()
+
+# Creat list for dictionaries of ref flow values
+new_rows = []
+for commodity in unique_commodities:
+    
+    # Get process uuid and name for each new ref flow
+    processID = df_olca[df_olca['Commodity'] == commodity]['ProcessID'].iloc[0]
+    processName = df_olca[df_olca['Commodity'] == commodity]['ProcessName'].iloc[0]
+    
+    # Create FlowName by modifying the commodity string
+    flowName = f"{commodity}, transported"
+    
+    # generate reference flow uuid
+    flowUUID = make_uuid([flowName, processName, processID])
+
+    # Create the new row as a dictionary
+    new_row = {
+        'Commodity': commodity,
+        'ProcessID': processID,
+        'ProcessName': processName,
+        'FlowName': flowName,
+        'FlowUUID': flowUUID,
+        'IsInput': False,
+        'reference': True,
+        'amount': 1.0,
+        'unit': 'kg',
+        'default_provider': 'nan',
+        'default_provider_name': 'nan'
+    }
+    new_rows.append(new_row)
+
+# Convert new rows to DataFrame
+new_df = pd.DataFrame(new_rows)
+
+# Append to original DataFrame
+df_olca = pd.concat([df_olca, new_df], ignore_index=True)
+
+
+#%% Add values shared by both inputs and ref flow
+
+# *** Do we need to add to this? Will a new folder path create a new olca category in the app?
+df_olca['ProcessCategory'] = 'Technosphere / 48-49: Transportation and Warehousing'
+# *** Do we need to add to flow category?
+df_olca['Context'] = 'Technosphere Flows / 48-49: Transportation and Warehousing'
+df_olca['FlowType'] = 'PRODUCT_FLOW'
+df_olca['avoided_product'] = False
+
+
+#%% Assign exchange dqi
+from flcac_utils.util import format_dqi_score
+df_olca['exchange_dqi'] = format_dqi_score(meta['DQI']['Flow'])
+
+
+#%% Assign locations to processes
+
+### Follow up on the purpose of this code block 
+from flcac_utils.util import generate_locations_from_exchange_df
+from esupy.location import read_iso_3166
+
+
+# *** Can I just assign location to be US?
+df_olca = df_olca.merge(read_iso_3166()
+                            .filter(['ISO-2d', 'ISO-3d'])
+                            .rename(columns={'ISO-3d': 'CountryCode',
+                                             'ISO-2d': 'location'}),
+                        how='left')
+locations = generate_locations_from_exchange_df(df_olca)
+
+
+#%% Build supporting objects
+from flcac_utils.generate_processes import build_location_dict
+from flcac_utils.util import extract_actors_from_process_meta, \
+    extract_sources_from_process_meta, extract_dqsystems
+
+(process_meta, source_objs) = extract_sources_from_process_meta(
+    process_meta, bib_path = working_dir / 'transport_sources.bib')
+
+(process_meta, actor_objs) = extract_actors_from_process_meta(process_meta)
+
+dq_objs = extract_dqsystems(meta['DQI']['dqSystem'])
+
+process_meta['dq_entry'] = format_dqi_score(meta['DQI']['Process'])
+
+# generate dictionary of location objects
+location_objs = build_location_dict(df_olca, locations)
+
+
+#%% Create json file
+from flcac_utils.generate_processes import build_flow_dict, \
+    build_process_dict, write_objects, validate_exchange_data
+from flcac_utils.util import assign_year_to_meta
+
+validate_exchange_data(df_olca)
+flows, new_flows = build_flow_dict(df_olca)
+processes = {}
+for year in df_olca.Year.unique():
+    ### *** I dont think this is relevant since we have 1 year of data
+    process_meta = assign_year_to_meta(process_meta, year)
+    # Update time period to match year for each region
+
+    p_dict = build_process_dict(df_olca.query('Year == @year'),
+                                flows,
+                                meta=process_meta,
+                                loc_objs=location_objs,
+                                source_objs=source_objs,
+                                actor_objs=actor_objs,
+                                dq_objs=dq_objs,
+                                )
+    processes.update(p_dict)
+
+write_objects('international_electricity', flows, new_flows, processes,
+              location_objs, source_objs, actor_objs, dq_objs,
+              )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
